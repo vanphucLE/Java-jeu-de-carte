44edf7afb2ed07bcf998398279eb5275090e159c
Lê <L@LEVPhuc>
2016-12-04 18:27:29 +0100

    en train de faire joueurphysique

--- lo02/src/com/sdz/modele/JoueurPhysique.java
+++ lo02/src/com/sdz/modele/JoueurPhysique.java
@@ -19,6 +19,7 @@ public class JoueurPhysique extends Joueur {
 	}
 	//Un joueur veut défausser un plusieur Cartes
 	private void seDefausserCartes(JeuDeCartes jeuDeCartes){
+		System.out.println("Votre point Action : "+this.ptAction+" ---- Origine : "+this.ptActionOrigine);
 		Scanner sc= new Scanner(System.in);
 		String commande="";
 		System.out.print("Voulez-vous défausser les cartes ? (Y/N):");
@@ -26,6 +27,7 @@ public class JoueurPhysique extends Joueur {
 		if(commande == "Y"){
 			String commande2="";
 			System.out.println("Les cartes actions tenu dans vôtre main:");
+			System.out.println(this.laMain);
 			System.out.print("Choisir les Id dont les cartes actions déffausées (Ex: 1 3 5) : ");
 			commande2=sc.nextLine();
 			commande2=commande2.trim();
@@ -44,15 +46,17 @@ public class JoueurPhysique extends Joueur {
 	//Le joueur compléte 7 cartes dans la main
 	private void Compeleter7Carte(JeuDeCartes jeuDeCartes){
 		int compte = this.laMain.getListeCarteA().size();
-		while (compte <= 7) {
+		while (compte < 7) {
 			compte++;
 			CarteAction carte = jeuDeCartes.distribuerCarteAction();
 			this.getLaMain().completerCarteAction(carte);
 		}
-		System.out.println("Vous avez complété 7 Cartes. Maintenant, les 7 cartes sont: ");
-
+		System.out.println("Vous avez complété 7 Cartes. Maintenant, les cartes sont: ");
+		System.out.println(this.laMain);
 	}
 	public void choisirCarteReel() {
+		System.out.println("(Rappeler) Votre point Action : "+this.ptAction+" ---- Origine : "+this.ptActionOrigine);
+		System.out.print("Choissiez Id dont la carte pour jouer(Ex: 5): ");
 		
 	}
 
